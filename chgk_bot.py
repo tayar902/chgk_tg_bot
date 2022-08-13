9bd5674b3b5fa4cef2146e72a68fab843d229dd5
Albert <tayar0409@gmail.com>
2022-08-13 15:14:47 +0300

    исправления ввода

--- chgk_bot.py
+++ chgk_bot.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot('5420233585:AAFUeq6tn5a-DG3FkgqYEYEUF6aqR-MDoTM')
 
 
 @bot.message_handler(commands=['start'])
-def start(m, res=False):
+def start(m):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Вопрос ЧГК")
     markup.add(item1)
@@ -16,7 +16,7 @@ def start(m, res=False):
 
 
 outp = ''
-data = []
+check_input = []
 
 
 @bot.message_handler(content_types=["text"])
@@ -27,10 +27,10 @@ def handle_text(message):
         question = ''
         global outp
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        data.append(message.text.strip())
-        print(data)
+        check_input.append(message.text.strip())
+        print(check_input)
         if message.text.strip() == 'Вопрос ЧГК':
-            if data == ['Вопрос ЧГК']:
+            if check_input == ['Вопрос ЧГК']:
                 question = chgk_api.get_question(all_text)
                 outp = ''
                 outp = chgk_api.get_answer(all_text)
@@ -39,23 +39,23 @@ def handle_text(message):
                 markup.add(item2)
                 bot.send_message(message.chat.id, answer, reply_markup=markup)
             else:
-                del data[0]
+                del check_input[0]
             bot.send_message(message.chat.id, 'Нажмите "Ответ", чтобы '
                              + 'получить ответ')
         elif message.text.strip() == 'Ответ':
-            if data == ['Вопрос ЧГК', 'Ответ']:
-                item1 = types.KeyboardButton("Вопрос ЧГК")
-                markup.add(item1)
+            item1 = types.KeyboardButton("Вопрос ЧГК")
+            markup.add(item1)
+            if check_input == ['Вопрос ЧГК', 'Ответ']:
                 answer = outp
             else:
                 answer = 'Вы не запросили вопрос!'
-            data.clear()
+            check_input.clear()
             bot.send_message(message.chat.id, answer, reply_markup=markup)
         else:
-            bot.send_message(message.chat.id, 'Вы ввели незнакомую команду\n '
-                             + 'Нажмите "Вопрос ЧГК" для получения вопроса'
-                             + 'Нажмите "Ответ", чтобы получить ответ')
-            del data[-1]
+            bot.send_message(message.chat.id, 'Вы ввели незнакомую команду\n'
+                             + 'Нажмите "Вопрос ЧГК" для получения вопроса или'
+                             + ' нажмите "Ответ", чтобы получить ответ')
+            del check_input[-1]
     except ConnectionError:
         with open('log.txt', 'a') as output_f:
             output_f.write(asctime() + ': ' + 'ConnectionError\n')
